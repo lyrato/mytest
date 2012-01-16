13a132363e78809761341c1e6a96f7f75f4c4340
lyrato <lyrato@gmail.com>
2012-01-16 18:57:08 +0800

    update test

--- rtl8187_dev.c
+++ rtl8187_dev.c
@@ -14,6 +14,8 @@
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
  */
+ //master update
+ //
 sdfsdfsdfee
 #include <linux/init.h>
 #include <linux/usb.h>
